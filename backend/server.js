e394026099419a97aaf911010a7b19a44304cff4
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-04-23 05:59:15 +0300

    Setup server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,42 @@
 import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
 
 const app = express();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+    console.log(response);
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+});
 
 app.listen(8000, () => {
-  console.log("hello");
+  console.log("App is running");
 });
