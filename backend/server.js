8ab128a17f8389f1402cd2fe688ac517be23c283
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-04-25 01:40:55 +0300

    Change place of import express

--- backend/server.js
+++ backend/server.js
@@ -4,35 +4,34 @@ import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
-
-const app = express();
-const configuration = new Configuration({
+const configration = new Configuration({
   apiKey: process.env.OPENAI_KEY,
 });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi(configration);
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/", async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const question = req.body.question;
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `${question}`,
       temperature: 0,
       max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
-    console.log(response);
+    // console.log(response);
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
   } catch (error) {
-    console.error(error);
+    // console.error(error);
     res.status(500).send(error || "Something went wrong");
   }
 });
