29b2b7cff6c4566c62adf76173685d6a595d59f8
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-04-25 10:47:41 +0300

    Add page chat component to chat view

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -2,53 +2,53 @@
   <div class="page-wrapper">
     <v-container fluid fill-height>
       <v-layout column>
-        <v-flex grow v-if="messageSent">
-          <div class="message-box mb-3">
-            <v-avatar class="mr-2 mb-2">
-              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-            </v-avatar>
-            {{ message }}
-          </div>
-          <div class="message-box">
-            <v-avatar class="mr-2 mb-2">
-              <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-            </v-avatar>
-            Thanks
+        <v-flex grow style="margin-bottom: 150px">
+          <div id="chat_container">
+            <div
+              v-for="(chat, i) in wrapper"
+              :key="i"
+              class="wrapper"
+              :class="{ ai: chat.isAi }"
+            >
+              <page-chat :chat="chat" :key="i" />
+            </div>
           </div>
         </v-flex>
         <v-flex shrink>
-          <v-card color="grey lighten-4">
-            <v-card-text>
-              <v-container>
-                <v-row class="textarea">
-                  <v-col cols="12">
-                    <v-row no-gutters>
-                      <v-col cols="11">
-                        <v-textarea
-                          style="background-color: white"
-                          outlined
-                          hide-details
-                          v-model="message"
-                          :rows="1"
-                          placeholder="Type your message here"
-                        ></v-textarea>
-                      </v-col>
-                      <v-col cols="1" align-self-center>
-                        <v-btn
-                          @click="sendMessage"
-                          color="primary"
-                          class="mt-2 ml-2"
-                          icon
-                        >
-                          <v-icon>mdi-send</v-icon>
-                        </v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-          </v-card>
+          <form @submit.prevent="fetchAnswer">
+            <v-card color="grey lighten-4">
+              <v-card-text>
+                <v-container>
+                  <v-row class="textarea">
+                    <v-col cols="12">
+                      <v-row no-gutters>
+                        <v-col cols="11">
+                          <v-textarea
+                            outlined
+                            style="background-color: white"
+                            hide-details
+                            v-model="question"
+                            :rows="1"
+                            placeholder="Ask ChatCoder"
+                          ></v-textarea>
+                        </v-col>
+                        <v-col cols="1" align-self-center>
+                          <v-btn
+                            type="submit"
+                            color="primary"
+                            class="mt-2 ml-2"
+                            icon
+                          >
+                            <v-icon>mdi-send</v-icon>
+                          </v-btn>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+            </v-card>
+          </form>
         </v-flex>
       </v-layout>
     </v-container>
@@ -56,24 +56,57 @@
 </template>
 
 <script>
+import PageChat from "../components/PageChat.vue";
 export default {
+  components: { PageChat },
   data() {
     return {
-      message: "",
-      messageSent: false,
+      question: "",
+      wrapper: [],
+      loading: false,
     };
   },
   methods: {
-    sendMessage() {
-      this.messageSent = true;
+    async fetchAnswer() {
+      try {
+        this.loading = true;
+        this.wrapper.push({
+          isAi: false,
+          value: this.question,
+        });
+        this.wrapper.push({
+          isAi: true,
+          value: "Loading...",
+        });
+        const res = await fetch("http://localhost:8000", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            question: this.question,
+          }),
+        });
+        const data = await res.json();
+        const parsedData = data.bot.trim();
+        this.wrapper.pop();
+        this.wrapper.push({
+          isAi: true,
+          value: parsedData,
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        this.loading = false;
+        this.question = "";
+      }
     },
   },
 };
 </script>
-
 <style scoped>
 .page-wrapper {
-  max-width: 840px;
+  max-width: 900px;
   margin: 0 auto;
 }
 
@@ -90,9 +123,18 @@ export default {
   max-width: 900px;
 }
 
-.message-box {
-  background-color: #f5f5f5; /* add gray background to message box */
-  padding: 10px;
-  width: 100%; /* add width property to make message box take full width */
+.chat-container {
+  height: calc(100% - 80px);
+  overflow-y: auto;
+  padding: 20px;
+}
+
+.wrapper {
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.ai {
+  background-color: #f5f5f5;
 }
 </style>
