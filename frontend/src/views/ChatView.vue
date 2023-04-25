2a786264a48e43fc7f7c2056c36a1388d3faf38f
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-04-25 05:43:41 +0300

    Add page wrapper to chat view

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,44 +1,46 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout column>
-      <v-flex grow>
-        <!-- Your main content goes here -->
-      </v-flex>
-      <v-flex shrink>
-        <v-card color="grey lighten-4">
-          <v-card-text>
-            <v-container>
-              <v-row class="area">
-                <v-col cols="12">
-                  <v-row no-gutters>
-                    <v-col cols="11">
-                      <v-textarea
-                        outlined
-                        hide-details
-                        v-model="message"
-                        :rows="1"
-                        placeholder="Type your message here"
-                      ></v-textarea>
-                    </v-col>
-                    <v-col cols="1" align-self-center>
-                      <v-btn
-                        @click="sendMessage"
-                        color="primary"
-                        class="mt-2 ml-2"
-                        icon
-                      >
-                        <v-icon>mdi-send</v-icon>
-                      </v-btn>
-                    </v-col>
-                  </v-row>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <div class="page-wrapper">
+    <v-container fluid fill-height>
+      <v-layout column>
+        <v-flex grow>
+          <!-- Your main content goes here -->
+        </v-flex>
+        <v-flex shrink>
+          <v-card color="grey lighten-4">
+            <v-card-text>
+              <v-container>
+                <v-row class="textarea">
+                  <v-col cols="12">
+                    <v-row no-gutters>
+                      <v-col cols="11">
+                        <v-textarea
+                          outlined
+                          hide-details
+                          v-model="message"
+                          :rows="1"
+                          placeholder="Type your message here"
+                        ></v-textarea>
+                      </v-col>
+                      <v-col cols="1" align-self-center>
+                        <v-btn
+                          @click="sendMessage"
+                          color="primary"
+                          class="mt-2 ml-2"
+                          icon
+                        >
+                          <v-icon>mdi-send</v-icon>
+                        </v-btn>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -60,6 +62,11 @@ export default {
 </script>
 
 <style scoped>
+.page-wrapper {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
 .v-card {
   position: fixed;
   bottom: 0;
@@ -68,8 +75,8 @@ export default {
   z-index: 1;
 }
 
-.area {
+.textarea {
   margin: auto;
-  max-width: 1000px;
+  max-width: 900px;
 }
 </style>
